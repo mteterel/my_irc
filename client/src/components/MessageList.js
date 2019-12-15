f8a09192af7ee41a660b3d83c300607fe493c9d1
Mérill T <merill.teterel@epitech.eu>
2019-12-15 22:03:45 +0100

    Cosmetic changes :)

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -12,7 +12,7 @@ class MessageList extends Component {
                         <List.Item>
                             {item.type === "system" && <span style={{fontWeight: "bold"}}>{item.content}</span>}
                             {item.type === "system_error" && <span style={{fontWeight: "bold", color: "red"}}>{item.content}</span>}
-                            {item.type === "user" && <><span style={{fontWeight: "bold"}}>{item.from}:</span><span>{item.content}</span></>}
+                            {item.type === "user" && <><span style={{fontWeight: "bold"}}>{item.from}: </span><span>{item.content}</span></>}
                             {item.type === "whisper" && <><span style={{fontWeight: "bold", color:"magenta"}}>from [{item.from}]: {item.content}</span></>}
                         </List.Item>
                     )}
