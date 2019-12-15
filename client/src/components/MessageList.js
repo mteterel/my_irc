2b565b2272849cee7f5a93a92543f4a0fb74528e
Mérill T <merill.teterel@epitech.eu>
2019-12-15 22:58:12 +0100

    Emoji support

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Button, List} from "antd";
+import Emojify from "react-emojione";
 
 class MessageList extends Component {
     render() {
@@ -11,9 +12,16 @@ class MessageList extends Component {
                     renderItem={item => (
                         <List.Item>
                             {item.type === "system" && <span style={{fontWeight: "bold"}}>{item.content}</span>}
-                            {item.type === "system_error" && <span style={{fontWeight: "bold", color: "red"}}>{item.content}</span>}
-                            {item.type === "user" && <><span style={{fontWeight: "bold"}}>{item.from}: </span><span>{item.content}</span></>}
-                            {item.type === "whisper" && <><span style={{fontWeight: "bold", color:"magenta"}}>from [{item.from}]: {item.content}</span></>}
+                            {item.type === "system_error" &&
+                            <span style={{fontWeight: "bold", color: "red"}}>{item.content}</span>}
+                            {item.type === "user" && <><span
+                                style={{fontWeight: "bold"}}>{item.from}: </span><span><Emojify
+                                style={styles.emoji}>{item.content}</Emojify></span></>}
+                            {item.type === "whisper" && <><span style={{
+                                fontWeight: "bold",
+                                color: "magenta"
+                            }}>from [{item.from}]: <Emojify
+                                style={{height: 24, width: 24}}>{item.content}</Emojify></span></>}
                         </List.Item>
                     )}
                 />
@@ -21,5 +29,12 @@ class MessageList extends Component {
         );
     }
 }
+
+const styles = {
+    emoji: {
+        height: 22,
+        width: 22
+    }
+};
 
 export default MessageList;
